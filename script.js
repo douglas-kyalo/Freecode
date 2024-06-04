ea14fdece13e66151344ac9fbb050ae99ebe7311
douglas <162567842+douglas-kyalo@users.noreply.github.com>
2024-06-04 17:01:25 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//1. decimal to binary converter
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
@@ -87,3 +89,41 @@ numberInput.addEventListener("keydown", (e) => {
     checkUserInput();
   }
 });
+
+
+
+
+
+
+
+
+//2. date formatter
+
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+      default:
+    currentDateParagraph.textContent = formattedDate;
+  }
